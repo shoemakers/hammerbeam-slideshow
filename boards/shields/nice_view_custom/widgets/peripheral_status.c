e2c491e9fdc809f401fa15029763662577b61a07
小邪降 <sshoemaker@163.com>
2026-03-14 08:02:34 +0800

    Update peripheral_status.c

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -25,34 +25,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 LV_IMG_DECLARE(hammerbeam1);
 LV_IMG_DECLARE(hammerbeam2);
-LV_IMG_DECLARE(hammerbeam3);
-LV_IMG_DECLARE(hammerbeam4);
-LV_IMG_DECLARE(hammerbeam5);
-LV_IMG_DECLARE(hammerbeam6);
-LV_IMG_DECLARE(hammerbeam7);
-LV_IMG_DECLARE(hammerbeam8);
-LV_IMG_DECLARE(hammerbeam9);
-LV_IMG_DECLARE(hammerbeam10);
-LV_IMG_DECLARE(hammerbeam11);
-LV_IMG_DECLARE(hammerbeam12);
-LV_IMG_DECLARE(hammerbeam13);
-LV_IMG_DECLARE(hammerbeam14);
-LV_IMG_DECLARE(hammerbeam15);
-LV_IMG_DECLARE(hammerbeam16);
-LV_IMG_DECLARE(hammerbeam17);
-LV_IMG_DECLARE(hammerbeam18);
-LV_IMG_DECLARE(hammerbeam19);
-LV_IMG_DECLARE(hammerbeam20);
-LV_IMG_DECLARE(hammerbeam21);
-LV_IMG_DECLARE(hammerbeam22);
-LV_IMG_DECLARE(hammerbeam23);
-LV_IMG_DECLARE(hammerbeam24);
-LV_IMG_DECLARE(hammerbeam25);
-LV_IMG_DECLARE(hammerbeam26);
-LV_IMG_DECLARE(hammerbeam27);
-LV_IMG_DECLARE(hammerbeam28);
-LV_IMG_DECLARE(hammerbeam29);
-LV_IMG_DECLARE(hammerbeam30);
+
 
 const lv_img_dsc_t *anim_imgs[] = {
     &hammerbeam1,
